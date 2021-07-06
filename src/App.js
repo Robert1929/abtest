9d6955112b4680e36b63ca243de886331e2d2cd7
Robert Gabriel Ursan <r.ursan@roislpt2298.levi9.com>
2021-07-06 16:16:14 +0300

    added element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { Experiment, Variant } from "react-optimize";
-let ceva = <div></div>;
-const handleOriginal = () => {
-  console.log("Original");
-  ceva = <div>Original</div>;
-};
-const handleVariant = () => {
-  console.log("Variant");
-  ceva = <div>Variant</div>;
-};
+
 function App() {
+  const [number, setNumber]=useState(0);
+  const handleOriginal = () => {
+    console.log("Original");
+    setNumber(number+1);
+  };
+  const handleVariant = () => {
+    console.log("Variant");
+    setNumber(number-1)
+  };
   return (
     <Experiment id="<experiment-id>">
       <Variant id="0">
         <button onClick={handleOriginal}>Original</button>
+        <div>Ai apasat de {number} ori;</div>
       </Variant>
       <Variant id="1">
         <button onClick={handleVariant}>Variant 1</button>
+        <div>Ai apasat de {number} ori;</div>
       </Variant>
-      {ceva}
     </Experiment>
   );
 }
