cd5d36148f13d41adfe2be7b38435ba62eadbd7b
Robert Gabriel Ursan <r.ursan@roislpt2298.levi9.com>
2021-07-07 08:37:24 +0300

    removed react-use

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import "./App.css";
 import React, { useState } from "react";
-import { Experiment, Variant } from "react-optimize";
 
 function App() {
-  const [number, setNumber]=useState(0);
+  const [number, setNumber] = useState(0);
   const handleOriginal = () => {
     console.log("Original");
-    setNumber(number+1);
+    setNumber(number + 1);
   };
   const handleVariant = () => {
     console.log("Variant");
-    setNumber(number-1)
+    setNumber(number - 1);
   };
   return (
-    <Experiment id="<experiment-id>">
-      <Variant id="0">
-        <button onClick={handleOriginal}>Original</button>
-        <div>Ai apasat de {number} ori;</div>
-      </Variant>
-      <Variant id="1">
-        <button onClick={handleVariant}>Variant 1</button>
-        <div>Ai apasat de {number} ori;</div>
-      </Variant>
-    </Experiment>
+    <div>
+      <button onClick={handleOriginal}>Original</button>
+      <div>Ai apasat de {number} ori;</div>
+
+      <button onClick={handleVariant}>Variant 1</button>
+      <div>Ai apasat de {number} ori;</div>
+    </div>
   );
 }
 
